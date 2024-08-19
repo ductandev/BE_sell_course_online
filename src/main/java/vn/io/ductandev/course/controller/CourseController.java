90e8c6c28320feb86a30310781a75ce0abca341f
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-08-19 15:48:38 +0700

    fixed: CORS Security source

--- src/main/java/vn/io/ductandev/course/controller/CourseController.java
+++ src/main/java/vn/io/ductandev/course/controller/CourseController.java
@@ -7,11 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import vn.io.ductandev.course.dto.CourseDTO;
 import vn.io.ductandev.course.dto.ICourseTopSale;
@@ -21,6 +17,7 @@ import vn.io.ductandev.course.service.CourseService;
 @Tag(name = "Course")
 @RestController
 @RequestMapping("/api/v1/course")
+@CrossOrigin
 public class CourseController {
 	
 	@Autowired
--- src/main/java/vn/io/ductandev/course/security/SecurityConfig.java
+++ src/main/java/vn/io/ductandev/course/security/SecurityConfig.java
@@ -7,10 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration          // Cho phép file này chạy ở tầng config trước
 @EnableWebSecurity      // cho phép custom Security
@@ -23,29 +27,36 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();         // Tính đa hình
     };
 
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true); // Cho phép gửi cookie cùng với request
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
     // ===============================================
     //                  SECURITY
     // ===============================================
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())        // Tắt csrf đi theo dạng tấn công copy token
-                .cors(cors -> cors.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))       // Chặn session
-                .authorizeHttpRequests(request -> {                                      // authorizeHttpRequests: nơi quy định xem đường dẫn nào có quyền truy cập hoặc ko có quyền truy cập
-//                    // Cho phép truy cập vào Swagger UI mà không cần xác thực
-//                    request.requestMatchers("/swagger").permitAll();
-//                    request.requestMatchers("/v3/api-docs/**").permitAll();
-//
-//                    // request.requestMatchers(HttpMethod.POST,"/authen").permitAll();   // Phương thức POST Ko cần chứng thực
-//                    request.requestMatchers("/auth").permitAll();                    // link Ko cần chứng thực
-//                    request.requestMatchers("/product").hasRole("ADMIN");              // link này duy nhất có quyền admin mới sài dc
-//
-//                    request.anyRequest().authenticated();                                // Tất cả các link còn lại đều cần xác thực.
-
-                    // Cho phép tất cả các request mà không cần xác thực
-                    request.anyRequest().permitAll();
-                })
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.addAllowedOrigin("http://localhost:3000");
+                    configuration.addAllowedMethod("*");
+                    configuration.addAllowedHeader("*");
+                    configuration.setAllowCredentials(true);
+                    return configuration;
+                }))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
                 .build();
     }
 }
